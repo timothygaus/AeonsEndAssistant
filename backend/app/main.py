b133be23c34381602b617871805b1567a3aac1bf
timothygaus <gaus.timothy@gmail.com>
2026-05-29 19:35:43 -0400

    fix: update CORS middleware to allow multiple origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://34.201.143.206:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
